66d747a92c7dfcf85fd1aa88b8e3df1a46553910
CiaraMaria Proia <proiacm@gmail.com>
2020-04-04 17:57:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,10 @@
 class Anagram
 attr_accessor :word
-new_array = []
+#new_array = []
 
 def initialize(word)
   @word = word
-  new_array << @word
+  #new_array << @word
 end
 
 def match(new_array)
