24e67358bf34efc12e01464b029ed60ad7edf496
CiaraMaria Proia <proiacm@gmail.com>
2020-04-04 17:49:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,14 @@
 class Anagram
 attr_accessor :word
-new_array = [
-  ]
+new_array = []
+
 def initialize(word)
   @word = word
   new_array << @word
 end
 
-def match(array)
-  new_array.each do |word| word.sort
+def match(new_array)
+  new_array.select do |word| word.sort
 end
 
 end
