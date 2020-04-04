874e294a0525abc80d5203cf74a628ce30298bf1
CiaraMaria Proia <proiacm@gmail.com>
2020-04-04 16:51:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,14 @@ def initialize(word)
 end
 
 def match(possible_matches)
- %w(possible_matches)
-  if 
+ %w(possible_matches).split
+ possible_matches.each do |word|
+    if
+      word == word
+      return true 
+    else
+      possible_matches = []
+  end
 end
 
 end
