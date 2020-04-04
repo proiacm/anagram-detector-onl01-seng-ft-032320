990c6d6793571c2732ca1b39d04b78d67ea2f1e3
CiaraMaria Proia <proiacm@gmail.com>
2020-04-04 18:00:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,14 @@
 class Anagram
 attr_accessor :word
-#new_array = []
 
 def initialize(word)
   @word = word
-  #new_array << @word
 end
 
 def match(new_array)
   new_array.select do |word| 
-    word.split(" ").sort == @word.split(" ").sort
-      if 
-end
-
-end
+    word.split("").sort == @word.split("").sort
+    end
+  end
 
 end
